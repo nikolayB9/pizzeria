956a6101c30853627597d9bbb2261b68188ac965
nikolayB9 <nkpir22@gmail.com>
2025-04-29 19:22:32 +0300

    update Category, Parameter

--- src/laravel/app/Models/Parameter.php
+++ src/laravel/app/Models/Parameter.php
@@ -8,8 +8,6 @@ use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Parameter extends Model
 {
-    use HasFactory;
-
     protected $guarded = ['id'];
     public $timestamps = false;
 
