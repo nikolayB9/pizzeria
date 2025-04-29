bdb1ae86384c4ce7e9ca5691bf4f86eb7151c542
nikolayB9 <nkpir22@gmail.com>
2025-04-29 10:10:01 +0300

    update run dev

--- npm-run-dev.sh
+++ npm-run-dev.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
+# Проверяем, есть ли файлы в node_modules внутри контейнера
+if ! docker compose exec --user appuser app test "$(docker compose exec --user appuser app sh -c 'ls -A /var/www/html/node_modules')"; then
+    echo "node_modules пустой. Выполняем npm install..."
+    docker compose exec --user appuser app npm install
+else
+    echo "node_modules не пустой. Пропускаем npm install."
+fi
+
 docker compose exec --user appuser app npm run dev
