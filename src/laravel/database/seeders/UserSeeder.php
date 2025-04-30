84638e1abb392a3dc64e0922ec9807b744686200
nikolayB9 <nkpir22@gmail.com>
2025-04-30 14:19:57 +0300

    update UserSeeder

--- src/laravel/database/seeders/UserSeeder.php
+++ src/laravel/database/seeders/UserSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Enums\User\UserRoleEnum;
 use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
 
 class UserSeeder extends Seeder
 {
@@ -12,19 +13,22 @@ class UserSeeder extends Seeder
 
     public function run(): void
     {
-        $this->createAdmin();
+        $this->updateOrCreateAdmin();
         $this->createUsers();
     }
 
-    private function createAdmin(): void
+    private function updateOrCreateAdmin(): void
     {
-        User::factory()->create([
-            'name' => 'Admin_1',
-            'phone_number' => '+79876543210',
-            'email' => 'admin@mail.ru',
-            'password' => '123123123',
-            'role' => UserRoleEnum::Admin->value,
-        ]);
+        User::updateOrCreate(
+            ['phone_number' => '+79876543210', 'email' => 'admin@mail.ru'],
+            [
+                'name' => 'Admin_1',
+                'password' => '123123123',
+                'role' => UserRoleEnum::Admin->value,
+                'birth_date' => fake()->randomElement([null, fake()->dateTimeBetween('-70 years', '-16 years')->format('Y-m-d')]),
+                'email_verified_at' => now(),
+                'remember_token' => Str::random(10),
+            ]);
     }
 
     private function createUsers(): void
