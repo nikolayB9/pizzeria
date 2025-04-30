6ac983f9e855041bde1eb7768a1d9ad7809a2630
nikolayB9 <nkpir22@gmail.com>
2025-04-30 14:36:30 +0300

    add cities

--- src/laravel/database/seeders/DatabaseSeeder.php
+++ src/laravel/database/seeders/DatabaseSeeder.php
@@ -22,6 +22,7 @@ class DatabaseSeeder extends Seeder
             CategoryProductSeeder::class,
             ProductVariantSeeder::class,
             ParameterProductVariantSeeder::class,
+            CitySeeder::class,
         ]);
     }
 }
