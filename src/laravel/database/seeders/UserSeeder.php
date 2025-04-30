648e9a4af7c9f5955553d8fb5824e68667e6f5d1
nikolayB9 <nkpir22@gmail.com>
2025-04-30 19:41:30 +0300

    update UserSeeder

--- src/laravel/database/seeders/UserSeeder.php
+++ src/laravel/database/seeders/UserSeeder.php
@@ -9,30 +9,41 @@ use Illuminate\Support\Str;
 
 class UserSeeder extends Seeder
 {
-    private int $userCount = 3;
-
     public function run(): void
     {
-        $this->updateOrCreateAdmin();
-        $this->createUsers();
+        $this->createAdmin();
+        $this->createUser();
     }
 
-    private function updateOrCreateAdmin(): void
+    private function createAdmin(): void
     {
-        User::updateOrCreate(
-            ['phone_number' => '+79876543210', 'email' => 'admin@mail.ru'],
-            [
+        if (!User::where('email', 'admin@mail.ru')->exists()) {
+            User::create([
                 'name' => 'Admin_1',
+                'phone_number' => '+79876543210',
+                'email' => 'admin@mail.ru',
+                'email_verified_at' => now(),
                 'password' => '123123123',
                 'role' => UserRoleEnum::Admin->value,
                 'birth_date' => fake()->randomElement([null, fake()->dateTimeBetween('-70 years', '-16 years')->format('Y-m-d')]),
-                'email_verified_at' => now(),
                 'remember_token' => Str::random(10),
             ]);
+        }
     }
 
-    private function createUsers(): void
+    private function createUser(): void
     {
-        User::factory($this->userCount)->create();
+        if (!User::where('email', 'user@mail.ru')->exists()) {
+            User::create([
+                'name' => 'User_1',
+                'phone_number' => '+79012345678',
+                'email' => 'user@mail.ru',
+                'email_verified_at' => now(),
+                'password' => '123123123',
+                'role' => UserRoleEnum::User->value,
+                'birth_date' => fake()->randomElement([null, fake()->dateTimeBetween('-70 years', '-16 years')->format('Y-m-d')]),
+                'remember_token' => Str::random(10),
+            ]);
+        }
     }
 }
