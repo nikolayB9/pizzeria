66c1981003abd4b664b481ebe130cb4c00a70d70
nikolayB9 <nkpir22@gmail.com>
2025-05-02 17:13:51 +0300

    site: update sanctum settings

--- src/laravel/routes/api.php
+++ src/laravel/routes/api.php
@@ -5,18 +5,27 @@ use App\Http\Controllers\Api\V1\Auth\RegisteredUserController;
 use App\Http\Controllers\Api\V1\CartController;
 use App\Http\Controllers\Api\V1\ProductController;
 use Illuminate\Support\Facades\Route;
+use Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful;
 
 // Роуты api (/api/v1/...)
-Route::prefix('v1')->group(function () {
+Route::middleware([
+    // Нужен, чтобы Sanctum не требовал токен для каждого запроса и использовал куки.
+    EnsureFrontendRequestsAreStateful::class,
+
+    // Обеспечивает доступ к сессии, кукам, CSRF и другой функциональности, необходимой для авторизации через куки
+    'web',
+])->prefix('v1')->group(function () {
+
+    // Публичные маршруты
     Route::get('/products', [ProductController::class, 'index']);
     Route::get('/products/{productSlug}', [ProductController::class, 'show']);
 
     Route::post('/cart', [CartController::class, 'store']);
 
-    Route::post('/register', [RegisteredUserController::class, 'store'])
-        ->middleware('guest');
-    Route::post('/login', [AuthenticatedSessionController::class, 'store'])
-        ->middleware('guest');
-    Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])
-        ->middleware('auth');
+    // Защищенные маршруты
+
+    // Авторизация
+    Route::post('/login', [AuthenticatedSessionController::class, 'store'])->middleware('guest');
+    Route::post('/register', [RegisteredUserController::class, 'store'])->middleware('guest');
+    Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])->middleware('auth');
 });
